eac1396fbf3c8b641bb32da4ab6ac1ec08e605eb
Artolex <kemalne_yo@hotmail.com>
2022-01-21 13:53:49 +0300

    third commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+# Oluşturduğumuz modeli yukarda import edip aşağıda register ediyoruz.
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_date", "isPublished")
+    list_display_links = ("id", "name")  # name link olmaları için
+    list_filter = ("created_date",)  # gün ay vb için filtre ekler.
+    # Vizyonda mı diye buton ekler ve değiştirilebilir.
+    list_editable = ("isPublished",)
+    search_fields = ("name", "description")  # arama fieldı ekler
+    list_per_page = 20  # 20 kayıtta yeni sayfa açar.
+
+# admin panelinde gösterimi özelleştirmek için. Modelimizdeki fieldları kullanabiliriz.
+
+
+admin.site.register(Movie, MovieAdmin)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# models.Model den türemesi gerekiyor bu classın.
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Film Adı")
+    description = models.TextField(verbose_name="Film Açıklaması")
+    image = models.CharField(max_length=50, verbose_name="Film Resmi")
+    created_date = models.DateTimeField(
+        auto_now_add=True, verbose_name="Eklenme Tarihi")
+    isPublished = models.BooleanField(default=True, verbose_name="Vizyonda")
+
+    def __str__(self):
+        return self.name
+    # Obje çağırıldığında yönetim panelinde __str__ objenin ismi olduğu için direk adını yazmasını sağlarız
